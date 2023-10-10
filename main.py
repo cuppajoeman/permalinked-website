9a5cd8f2145a0f5c36e6b73dd1ea700d03b9e70b
cuppajoeman <ccn@cuppajoeman.com>
2023-10-10 13:19:09 +0200

    Update to load file after window loads

--- main.py
+++ main.py
@@ -17,18 +17,30 @@ def get_content_directory():
 def add_uuid_to_files_without_one():
     file_path_to_new_path = {}
     for dir_path, dirs, file_names in walk(content_directory):
-        if "permalinked-website" not in dir_path:
-            for name in file_names:
 
-                uuid_already_in_filename = "UUID_" in name
+        if "permalinked-website" in dir_path:
+            continue
 
-                if not uuid_already_in_filename:
-                    full_path = os.path.join(dir_path, name)
-                    filename_no_extension, extension = os.path.splitext(name)
-                    generated_UUID = uuid.uuid4()
-                    new_path = os.path.join(dir_path, f"{filename_no_extension}_UUID_{generated_UUID}{extension}")
 
-                    file_path_to_new_path[full_path] = new_path
+        for name in file_names:
+            print(dir_path, name)
+
+            relative_path = os.path.relpath(dir_path, content_directory)
+
+            current_file_is_root_index = relative_path == "." and name == "index.html"
+
+            if current_file_is_root_index:
+                continue
+
+            uuid_already_in_filename = "UUID_" in name
+
+            if not uuid_already_in_filename:
+                full_path = os.path.join(dir_path, name)
+                filename_no_extension, extension = os.path.splitext(name)
+                generated_UUID = uuid.uuid4()
+                new_path = os.path.join(dir_path, f"{filename_no_extension}_UUID_{generated_UUID}{extension}")
+
+                file_path_to_new_path[full_path] = new_path
 
     for file_path, new_path in file_path_to_new_path.items():
         os.rename(file_path, new_path)
@@ -37,19 +49,21 @@ def add_uuid_to_files_without_one():
 def generate_uuid_to_paths():
     uuid_to_path = {}
     for dir_path, dirs, file_names in walk(content_directory):
-        if "permalinked-website" not in dir_path:
-            for name in file_names:
-                if (match := re.search(r"UUID_(.*?)\.", name)) is not None:
-                    full_path = os.path.join(dir_path, name)
-                    relative_path = os.path.relpath(full_path, content_directory)
-                    uuid_in_name = match.group(1)
-                    uuid_to_path[uuid_in_name] = relative_path
+
+        if "permalinked-website" in dir_path:
+            continue
+
+        for name in file_names:
+            if (match := re.search(r"UUID_(.*?)\.", name)) is not None:
+                full_path = os.path.join(dir_path, name)
+                relative_path = os.path.relpath(full_path, content_directory)
+                uuid_in_name = match.group(1)
+                uuid_to_path[uuid_in_name] = relative_path
 
     return uuid_to_path
 
 
 def write_to_json(data):
-    os.chdir("..")
     with open('uuid_to_paths.json', 'w') as fp:
         json.dump(data, fp, indent=4)
 
@@ -63,5 +77,7 @@ if __name__ == "__main__":
     content_directory = os.getcwd()
 
     add_uuid_to_files_without_one()
+     
+    os.chdir(script_directory)
     # we know all files now have a uuid with them
     write_to_json(generate_uuid_to_paths())
--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const urlParams = new URLSearchParams(window.location.search);
-const requestedUUID = urlParams.get('uuid');
+window.onload = function() {
+	const urlParams = new URLSearchParams(window.location.search);
+	const requestedUUID = urlParams.get('uuid');
 
-if (requestedUUID != null) {
-    fetch("uuid_to_paths.json")
-        .then(response => response.json())
-        .then(json => {
-            if (requestedUUID in json) {
-                let contentPath = json[requestedUUID];
-                window.location.href = contentPath;
-            } else {
-                document.body.innerText = "the request for a file with a uuid of : " + requestedUUID + " doesn't exist."
-            }
-        });
+	if (requestedUUID != null) {
+		fetch("/permalinked-website/" + "uuid_to_paths.json")
+			.then(response => response.json())
+			.then(json => {
+				if (requestedUUID in json) {
+					let contentPath = json[requestedUUID];
+					window.location.href = contentPath;
+				} else {
+					document.body.innerText = "the request for a file with a uuid of : " + requestedUUID + " doesn't exist."
+				}
+			});
 
+	}
 }
